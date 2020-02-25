26ad9c4bb6cfc9f41f0f748041afde5f5f0c7b01
Irina Baeva <irinabayova@gmail.com>
2020-02-25 22:48:45 +0100

    Request body validation with express-validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,47 @@
 const express = require('express')
-
 const router = express.Router()
-// @route GET api/auth
-//@desc Test route
-//@access public or provate
-router.get('/', (req, res) => res.send('User rout'))
+const gravatar = require('gravatar')
+
+const {
+    check,
+    validationResult
+} = require('express-validator');
+// bring user model
+const User = require('../../models/User')
+
+// @route   POST api/auth
+//@desc     Register user
+//@access   public or provate
+router.post('/', [
+        check('name', 'Name is required').not().isEmpty(),
+        check('email', 'Include valid email').isEmail(),
+        check('password', 'Please, enter a password with 6 or more characters').isLength({
+            min: 6
+        })
+
+    ],
+    async (req, res) => {
+        // Finds the validation errors in this request and wraps them in an object
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                errors: errors.array()
+            });
+        }
+        // Destructure from body
+        const {
+            name,
+            email,
+            password
+        } = req.body;
+        // See if user exist
+
+        // Get user gravatar (based on email)
+
+        // Encrypt password using
+
+        // Return jsonwebtocken
+        res.send('This is user rout')
+    })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require('express')
+const connectDB = require('./config/db')
+const {
+    check,
+    validationResult
+} = require('express-validator')
 const PORT = process.env.PORT || 5050;
 
 const app = express();
@@ -7,15 +11,17 @@ const app = express();
 //Connect DB
 connectDB();
 
-//define routes 
+//init middleware, before it was with BodyParser, now we can do with express
+app.use(express.json({
+    extended: false
+}))
+app.get('/', (req, res) => res.send('API is runnig'))
+
+//define routes
+app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/users', require('./routes/api/users'))
-
-
-
-app.get('/', (req, res) => res.send('API is runnig'))
 
 app.listen(PORT, () => {
     console.log(`Server listening ${PORT}`)
