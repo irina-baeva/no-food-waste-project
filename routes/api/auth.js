3bc1b7416a65b74e5dea675f42553d77e206560e
Irina Baeva <irina.baeva@bonial.com>
2020-03-02 22:34:19 +0100

    Add authentification logic wit jwt

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,20 @@
 const express = require('express')
-
 const router = express.Router()
+// we bring middleware auth
+const auth = require('../../middleware/auth')
+const User = require('../../models/User')
 // @route GET api/auth
 //@desc Test route
 //@access public or provate
-router.get('/', (req, res) => res.send('AUth rout'))
+//if we pass auth like 3rd parameter it is already make rout protected
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user)
+    } catch (err){
+        console.log(err)
+        res.status(500).send('Server error')
+    }
+})
 
 module.exports = router
