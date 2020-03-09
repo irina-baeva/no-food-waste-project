79b2c8f581797f12dfc121a445b9cd0fd20a0288
Irina Baeva <irina.baeva@bonial.com>
2020-03-09 15:59:07 +0100

    Add simple profile fields

---
+++ models/Profile.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const ProfileSchema = new mongoose.Schema({
+    //now we want to create reference to user model, cuz every profile should be assootioted with user
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    location:{
+        type: String
+    },
+    bio:{
+        type: String
+    },
+    position:{
+        type: String
+    },
+    social: {
+        linkedin:{
+            type: String
+        },
+        twitter:{
+            type: String
+        },
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+})
+//module.exports= Profile = mongoose.model('profile', profileSchema )
+const Profile = mongoose.model('profile', ProfileSchema)
+module.exports = Profile
