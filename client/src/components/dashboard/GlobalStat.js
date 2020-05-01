836937bab1cfc8ad0cbae679fab7e3c07ab3a197
Irina Baeva <irinabayova@gmail.com>
2020-05-01 21:42:42 +0200

    Added data on map

--- client/src/components/dashboard/GlobalStat.js
+++ client/src/components/dashboard/GlobalStat.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
-import * as countryData from "./data/countries.json";
+import * as countryData from "./data/data_countries_copy.json";
 
 function GlobalStat() {
   const [viewport, setViewport] = useState({
@@ -10,7 +10,7 @@ function GlobalStat() {
     width: "100vw",
     height: "100vh",
   });
-  // const [selectedPark, setSelectedPark] = useState(null);
+  const [selectedCountry, setSelectedCountry] = useState(null);
 
   // useEffect(() => {
   //   const listener = (e) => {
@@ -32,13 +32,30 @@ function GlobalStat() {
       >
         {countryData.features.map((country) => (
           <Marker
-            key="country.properties.country"
-            latitude={parseInt(country.properties.latitude)}
-            longitude={parseInt(country.properties.longitude)}
+            key="country.properties.id"
+            latitude={parseInt(country.geometry.coordinates[1])}
+            longitude={parseInt(country.geometry.coordinates[0])}
           >
-            <p>country</p>
+            <button
+              className="marker-btn"
+              onClick={(e) => {
+                e.preventDefault();
+                setSelectedCountry(country);
+              }}
+            >
+              <img className="icon" src="./garbage.png"></img>
+            </button>
           </Marker>
         ))}
+        {selectedCountry ? (
+          <Popup
+            latitude={selectedCountry.geometry.coordinates[1]}
+            longitude={selectedCountry.geometry.coordinates[0]}
+            onClose={() => {
+              setSelectedCountry(null);
+            }}
+          ></Popup>
+        ) : null}
       </ReactMapGL>
     </div>
   );
